ba8c1caae904fc74e8a585667629276f3f63e913
Alex Jeffreys <aj@belugalearning.com>
2013-05-09 13:41:42 +0100

    Initial Commit

---
+++ Pinboard/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Pinboard
+//
+//  Created by Alex Jeffreys on 09/05/2013.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
